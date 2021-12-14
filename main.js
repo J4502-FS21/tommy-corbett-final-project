a9550b270e8bc80516ac5a88609d2a121f21a5b0
Tommycc99 <tcchkk@umsystem.edu>
2021-12-14 11:50:37 -0600

    quiz
    more quiz updates

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 "use strict";
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
   var x = document.getElementById("myLinks");
   if (x.style.display === "block") {
@@ -14,6 +13,9 @@ function check(){
 	var question1 = document.quiz.question1.value;
 	var question2 = document.quiz.question2.value;
 	var question3 = document.quiz.question3.value;
+	var question4 = document.quiz.question4.value;
+	var question5 = document.quiz.question5.value;
+	var question6 = document.quiz.question6.value;
 	var correct = 0;
 
 
@@ -26,20 +28,29 @@ function check(){
   	if (question3 == "$14 million") {
   		correct++;
   	}
+		if (question4 == "ANSWER") {
+  		correct++;
+		}
+		if (question5 == "ANSWER") {
+  		correct++;
+		}
+		if (question6 == "ANSWER") {
+  		correct++;
+		}
 
-	var pictures = ["quizwin.jpeg", "quizmeh.jfif", "quizlose.jpg"];
-	var messages = ["Great job!", "That's just okay", "You really need to do better"];
+	var pictures = ["quizwin.jpeg", "meh.png", "elimination.png"];
+	var messages = ["Great job! You're safe this week!", "That's just okay. Hopefully you won't be the house vote.", "That was bad. You're going into elimination this week."];
 	var score;
 
   	if (correct == 0) {
   		score = 2;
   	}
 
-  	if (correct > 0 && correct < 3) {
+  	if (correct > 0 && correct < 6) {
   		score = 1;
   	}
 
-  	if (correct == 3) {
+  	if (correct == 6) {
   		score = 0;
   	}
 
